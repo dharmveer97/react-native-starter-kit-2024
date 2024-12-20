1aef0c972bee8d1d232d28f9b3b042b50027b628
dharmveer97 <dvbangar8@gmail.com>
2024-12-21 03:36:46 +0530

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+export default{
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
